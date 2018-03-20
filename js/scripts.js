8b30c0b542c1989448708deb12d4ff268252ec78
CJ Rhone and Jim Palowski <cj@>
2018-03-20 09:57:09 -0700

    Added another function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
+  $(document).ready(function() {
   $(".clickable").click(function() {
     $(".walrus-showing").fadeToggle();
     $(".walrus-hidden").slideToggle();
+  $(".slidesUp").click(function(){
+    $(".walrus-showing").slideToggle();
+    $(".walrus-hidden").slideToggle();
+    });
   });
-});
+  });
